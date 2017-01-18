8eebab029b66c49ef0f5b8bdb8f19e10e19054bc
Adam Kelly <adam.guitar12@gmail.com>
2017-01-18 17:58:03 +0000

    Use Tripple Equals

--- src/controllers/realtimedata.js
+++ src/controllers/realtimedata.js
@@ -5,7 +5,7 @@ const getRealtimeData = (stopId, callback) => {
     let requestUrl = `${BASE_URL}/realtimebusinformation?stopid=${stopId}&format=json`;
 
     request(requestUrl, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             const returnedData = JSON.parse(body);
             const results = returnedData.results;
 
--- src/controllers/stopdata.js
+++ src/controllers/stopdata.js
@@ -5,7 +5,7 @@ const getStopData = (stopId, callback) => {
     let requestUrl = `${BASE_URL}/busstopinformation?stopid=${stopId}&format=json`;
 
     request(requestUrl, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             const returnedData = JSON.parse(body);
             // The stop is the first item in the results array
             const stopData = returnedData.results[0];
